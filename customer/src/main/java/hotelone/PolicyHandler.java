dfefaea5b1a4c327e8aaa39a4616894328ba3f14
hyojung64 <87048623+hyojung64@users.noreply.github.com>
2021-08-19 11:29:49 +0900

    Add files via upload

---
+++ customer/src/main/java/hotelone/PolicyHandler.java
@@ -0,0 +1,35 @@
+package hotelone;
+
+import hotelone.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PolicyHandler{
+    @StreamListener(KafkaProcessor.INPUT)
+    public void onStringEventListener(@Payload String eventString){
+
+    }
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverReserveAccepted_(@Payload ReserveAccepted reserveAccepted){
+
+        if(reserveAccepted.isMe()){
+            System.out.println("##### Kako Send (Reservation Accepted  : " + reserveAccepted.toJson());
+            System.out.println("Config Value : " + System.getenv("DATA1"));
+            System.out.println("Secret Value : " + System.getenv("PASSWD"));
+        }
+    }
+    @StreamListener(KafkaProcessor.INPUT)
+    public void wheneverPaymentCanceled_(@Payload PaymentCanceled paymentCanceled){
+
+        if(paymentCanceled.isMe()){
+            System.out.println("##### listener  : " + paymentCanceled.toJson());
+        }
+    }
+
+}
